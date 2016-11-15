23570e891cfbf2cea1bae88b9fd2ff52579b28cb
Owen Canning <1002798@student.sae.edu.au>
2016-11-15 12:01:30 +1100

    basic furnature interaction completed

--- Assets/Scripts/FurnatureInteractionScript.cs
+++ Assets/Scripts/FurnatureInteractionScript.cs
@@ -13,9 +13,18 @@ public class FurnatureInteractionScript : MonoBehaviour {
 	public GameObject hinge;
 //	public GameObject UI;
 
+	private float xPosition;
+	private float yPosition;
+	private float zPosition;
+
 
 	// Use this for initialization
 	void Start () {
+
+		xPosition = hinge.transform.position.x;
+		yPosition = hinge.transform.position.y;
+		zPosition = hinge.transform.position.z;
+
 		if (moveType == "left") {
 			openAngle = 80.0f;
 		} else if (moveType == "right") {
@@ -25,6 +34,19 @@ public class FurnatureInteractionScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (moveType == "slide") {
+			//open
+			if (Input.GetKeyDown (KeyCode.E) && colliding == true && open == false) {
+				hinge.transform.position = new Vector3 (xPosition - 1.0f, yPosition, zPosition);
+				open = true;
+			}
+			//close
+			else if (Input.GetKeyDown (KeyCode.E) && colliding == true && open == true) {
+				hinge.transform.position = new Vector3 (xPosition, yPosition, zPosition);
+				open = false;
+			}
+		}
 
 		if (moveType != "slide") {
 			//open
