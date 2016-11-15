0880ced2d1610252d7f5f7af0a99d1162e1067d7
ayylmaoRegine <1003126@student.sae.edu.au>
2016-11-15 12:30:38 +1100

    Main Menu
    Minimal

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitch : MonoBehaviour {
+
+	public string myScene;
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel (myScene);
+	}
+}
